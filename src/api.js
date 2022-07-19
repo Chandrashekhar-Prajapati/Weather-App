8cd28f59883ba0643b3d1dc4e0ef729c1c2fcf7e
Chandrashekhar <mechn1423@gmail.com>
2022-07-18 22:04:51 -0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd394c75528msh555b0b2c16abaecp1b36a2jsn1c31a2e82514',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '9c4454fd653496bb6f2bebef0e5d5874';
